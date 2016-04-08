6ba9949d0b4c0390a534b49fe3ada2de125cb54c
Shawn Michaud <shawnmne@hotmail.com>
2016-04-07 19:08:31 -0500

    patron built out

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,8 +1,8 @@
 class Patron < ActiveRecord::Base
 
 	validates :name, presence: true
-	validates :email_address, presence: true
-	validates :email_address, uniqueness: true
+	validates :email, presence: true
+	validates :email, uniqueness: true
 	validates_associated :books
 
 	has_many :books
--- main.rb
+++ main.rb
@@ -109,5 +109,50 @@ end
  	end 	
 
 
+### patrons
+get '/patrons' do 
+  @patrons = Patron.all
+  erb :patrons_index
+end	
 
+# New
+get '/patrons/new' do
+  @patron = Patron.new
+  erb :patrons_new
+end
+
+# Create
+post '/patrons' do
+  @patron = Patron.new(params)
+
+  if @patron.save
+    redirect to('/patrons')
+  else
+    erb :patrons_new
+  end
+end
+
+# Show
+get '/patrons/:id' do
+  @patron = Patron.find_by_id(params['id']) 
+  erb :patrons_show
+end
+
+
+# Edit
+ 
+ get '/patrons/:id/edit' do
+   @patron = Patron.find_by_id(params['id'])
+   erb :patrons_edit
+ end
+ 
+ post '/patrons/:id' do
+   @patron = Patron.find_by_id(params['id'])
+   if @patron.update_attributes(name: params['name'],
+                                email: params['email'])
+     redirect to("/patrons/#{@patron.id}")
+   else
+     erb :patrons_edit
+   end
+ 	end 	
 
